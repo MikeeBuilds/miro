4f47842e114f8b441b1344a50963b4eec82a7230
MikeeBuilds <citizensofandrometta@gmail.com>
2024-06-29 16:50:33 -0400

    initial commit

--- .history/app/page_20240629164856.tsx
+++ .history/app/page_20240629164856.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button size="lg" variant='destructive'>
+    <Button size="lg">
       Click Me
     </Button>
   );
